a3b3491952f07f80ef97870d945fa6099d3a8605
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2026-04-11 13:31:20 +0300

    Implement Event CRUD login, validation and storage setup

---
+++ app/Http/Requests/StoreEventRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreEventRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'event_date' => 'required|date|after:today',
+            'limit' => 'required|integer|min:1',
+            'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
+        ];
+    }
+}
